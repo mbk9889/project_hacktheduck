e3c2847cfea323c4b015d712e9ed4da238f62f5f
mbk9889 <149243765+mbk9889@users.noreply.github.com>
2023-10-28 16:25:43 +0100

    Add files via upload

---
+++ capital_project.py
@@ -0,0 +1,36 @@
+authJWT = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJuYmYiOjE2OTYwMzIwMDAsImFwaV9zdWIiOiJhMmRlMjUxNGRmZTAwYjM5OTFhNzg0NTY4NzJkZDJjOTE1YWY5NjgzOTc2ZDAyZTdiZjk5NTIzMWE2MDllMDVjMTcxNzIwMDAwMDAwMCIsInBsYyI6IjVkY2VjNzRhZTk3NzAxMGUwM2FkNjQ5NSIsImV4cCI6MTcxNzIwMDAwMCwiZGV2ZWxvcGVyX2lkIjoiYTJkZTI1MTRkZmUwMGIzOTkxYTc4NDU2ODcyZGQyYzkxNWFmOTY4Mzk3NmQwMmU3YmY5OTUyMzFhNjA5ZTA1YyJ9.NiTvZUyTERVaV7krUmkiRgXbXhAeWEEZHkaus8aJ5pTMG4RYbZSP5reayEl1xi8kHO9g8jOWkj6v8ygUqyuPGcsL2jtqTMen1kUhe5m2bO6qQmdOvF6leyQkf7qWeYC8pzcIYhCnII5ZAcJxs3Lx3F3o6Zqy3aLsP1OxmG_TWiyWfdfhdCO8dekvQxXnp61oYAT8BvMPTbvoh6g9xPPO3q5KyYEAeLDGiPNeFML6CaDpvrwH_gf_ilDZJ84jGXuRGN5q_BqUgJq8QXahrbhvzM1ewNsC48Ljt0jFVD1kErMn4AVzJUkpZhzT6RJUvWC0e-lVObJ85yDCcbNdKOn7Dw'
+full_dictionarry = {'Transactions':[]}
+
+import json
+import requests
+
+def generate_the_data():
+    global full_dictionarry
+    headers = {
+    'Authorization': f'Bearer {authJWT}',
+    'Content-Type': 'application/json',
+    'version': '1.0'
+    }
+    for i in range(4):
+        quantity = 25
+        payload = json.dumps({"quantity": quantity})
+        account_id = "95526223"
+
+        response = requests.post(f"https://sandbox.capitalone.co.uk/developer-services-platform-pr/api/data/transactions/accounts/{account_id}/create", headers=headers, data=payload).text
+        json_response = json.loads(response)
+
+
+        json_response.pop('newBalance')
+        for x in range(len(json_response["Transactions"])):
+            full_dictionarry["Transactions"].append(json_response["Transactions"][x])
+
+generate_the_data()
+print(full_dictionarry)
+
+
+
+
+
+
+# the new balance is the balance that that uer had after all his transactions ...
+# and for the original balance we can get it from the user account him self ... 
